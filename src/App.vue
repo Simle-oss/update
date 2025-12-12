ee3d4556b0840d79094e367df64af72c56268c09
Unavailability <57338056+Simle-oss@users.noreply.github.com>
2025-12-12 17:33:58 +0800

    chore: migrate to Vue 3 + Vite + Pinia + Element Plus (initial setup)

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <template>
-  <div id="app">
-    <keep-alive  :include="cachedViews">
-    <router-view></router-view>
-    </keep-alive>
-  </div>
+  <router-view />
 </template>
+
 <script>
 export default {
-  data() {
-    return {
-      cachedViews: [],  // 用来存放需要缓存的路由组件
-    }
-  },
-    watch: {
-    $route(to, from) {
-      if (to.meta.keepAlive) {
-        if (!this.cachedViews.includes(to.name)) {
-          this.cachedViews.push(to.name);
-        }
-      } else {
-        const index = this.cachedViews.indexOf(to.name);
-        if (index > -1) {
-          this.cachedViews.splice(index, 1);
-        }
-      }
-    }
-}
+  name: 'App'
 }
 </script>
+
+<style>
+/* global styles can go here */
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,13 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Layout from "@/layout";
-import view from "./module/view";
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
 
-Vue.use(VueRouter);
-
-/*
-对VueRouter原型链上的push、replace方法进行重写
-为了解决某些情况下控制台报 ‘Uncaught (in promise) undefined’的问题
-参考地址：https://github.com/vuejs/vue-router/issues/2881
-*/
-const originalPush = VueRouter.prototype.push;
-VueRouter.prototype.push = function push(location, onResolve, onReject) {
-  if (onResolve || onReject)
-    return originalPush.call(this, location, onResolve, onReject);
-  return originalPush.call(this, location).catch((err) => err);
-};
-/**
- * 所有页面都应该是一级页面，或者说必须直接渲染到layout上，否则没有缓存
- * name 必须与组件name一致，用于路由缓存功能
- * permission 为布尔时表示需要权限,默认值应该为 false，待后端返回相应的地址后会自动改为 true
- */
 const routes = [
-  {
-    path: "/login",
-    hidden: true,
-    component: () =>
-      import(/* webpackChunkName: "login" */ "@/views/Login.vue"),
-  },
-  {
-    path: "/",
-    component: Layout,
-    redirect: "/disclosure",
-    children: [...view,],
-  },
-  {
-    path: '/oauth',
-    component: () => import('@/views/Oauth.vue'),
-    hidden: true,
-  },
-  { path: "*", redirect: "/404" },
-];
+  { path: '/', name: 'Home', component: Home }
+]
 
-const router = new VueRouter({
-  mode: "hash",
-  base: process.env.BASE_URL,
-  routes,
-});
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
 
-export default router;
+export default router
